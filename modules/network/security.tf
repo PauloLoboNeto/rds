757265f20eb00871764100a28198a44e1d951b06
pauloloboneto <pauloloboneto@gmail.com>
2023-11-02 16:47:19 -0300

    fix: ajustando security group

--- modules/network/security.tf
+++ modules/network/security.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "rds_security_group" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.rds_vpc.cidr_block]
+    cidr_blocks = "0.0.0.0/0"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
   }
 }
